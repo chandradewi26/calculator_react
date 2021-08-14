1937600762a2f0dee2151e483594bf7b9097c2c9
chandradewi26 <chandradewi2691@gmail.com>
2021-08-14 19:43:20 +1000

    Finished Calculator's logic

--- src/Components2/Calculator.jsx
+++ src/Components2/Calculator.jsx
@@ -8,12 +8,16 @@ class Calculator extends Component {
     //This code will be executed once Calculator is called
     constructor() {
         super();
-        this.state={data: '000'}; //this is what is displayed in the screen display
+        this.state={data: ''}; //this is what is displayed in the screen display
     }
 
     calculate = () => {
         try {
             const result = eval(this.state.data); //  var a = eval("20/5") + "aaa <br>"; ~> 4aaa<br>
+            this.setState({data: result});
+        } catch (e) 
+        {
+            this.setState({data: 'error'});
         }
     }
 
@@ -34,32 +38,32 @@ class Calculator extends Component {
     render() { 
         return ( 
             <div className="Nana-Calculator">
-                <Screen data={this.state.data}/>
+                <Screen data={this.state.data} />
                 <Keypad>
-                    <Button label='C' value="clear" />
-                    <Button label='7' value="7" />
-                    <Button label='4' value="4" />
-                    <Button label='1' value="1" />
-                    <Button label='0' value="0" />
+                    <Button onClick={this.handleClick} label='C' value="clear" />
+                    <Button onClick={this.handleClick} label='7' value="7" />
+                    <Button onClick={this.handleClick} label='4' value="4" />
+                    <Button onClick={this.handleClick} label='1' value="1" />
+                    <Button onClick={this.handleClick} label='0' value="0" />
 
                     
-                    <Button label='/' value="/" />
-                    <Button label='8' value="8" />
-                    <Button label='5' value="5" />
-                    <Button label='2' value="2" />
-                    <Button label='.' value="." />
+                    <Button onClick={this.handleClick} label='/' value="/" />
+                    <Button onClick={this.handleClick} label='8' value="8" />
+                    <Button onClick={this.handleClick} label='5' value="5" />
+                    <Button onClick={this.handleClick} label='2' value="2" />
+                    <Button onClick={this.handleClick} label='.' value="." />
 
                     
-                    <Button label='x' value="*" />
-                    <Button label='9' value="9" />
-                    <Button label='6' value="6" />
-                    <Button label='3' value="3" />
-                    <Button label=''  value="null" />
+                    <Button onClick={this.handleClick} label='x' value="*" />
+                    <Button onClick={this.handleClick} label='9' value="9" />
+                    <Button onClick={this.handleClick} label='6' value="6" />
+                    <Button onClick={this.handleClick} label='3' value="3" />
+                    <Button onClick={this.handleClick} label=''  value="null" />
 
                     
-                    <Button label='-' value="-" />
-                    <Button label='+' value="+" />
-                    <Button label='=' value="=" />
+                    <Button onClick={this.handleClick} label='-' value="-" />
+                    <Button onClick={this.handleClick} label='+' value="+" />
+                    <Button onClick={this.handleClick} label='=' value="equal" />
 
                 </Keypad>
             </div>
